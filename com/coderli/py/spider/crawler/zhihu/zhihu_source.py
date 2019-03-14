ce13b7041c2cb5e294b2426e25a7c158e4f15ef9
wushikezuo@gmail.com <wushikezuo@gmail.com>
2019-03-14 23:33:10 +0800

    analysis

---
+++ com/coderli/py/spider/crawler/zhihu/zhihu_source.py
@@ -0,0 +1,9 @@
+class Zhihu:
+
+
+    def login(self):
+        login_url = "https://www.zhihu.com/api/v4/mqtt/auth"
+        # {username: "zhihu_web/4d081309b0e60354584a7721a8abe38b", host: "mqtt.zhihu.com",…}
+        # host: "mqtt.zhihu.com"
+        # password: "1552314817/cf895ca57229f83b06c0d73929e4b165c7eb2bca"
+        # username: "zhihu_web/4d081309b0e60354584a7721a8abe38b"
