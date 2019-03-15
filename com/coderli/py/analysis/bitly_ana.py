5f5d1e150cfe1dcc4ad399b557f78d78baa71ead
lihongzhe <wushikezuo@gmail.com>
2019-03-15 16:39:40 +0800

    bitly demo

--- com/coderli/py/analysis/bitly_ana.py
+++ com/coderli/py/analysis/bitly_ana.py
@@ -1,1 +1,7 @@
 import json
+data_file_path = "bitly_usagov_example.txt";
+records = [json.loads(line) for line in open(data_file_path)]
+print(records[0]['tz'])
+
+time_zones = [rec['tz'] for rec in records if 'tz' in rec]
+print(time_zones[:10])
