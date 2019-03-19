0063d3b2b59b91afca28892b94ad4cce61310343
wushikezuo@gmail.com <wushikezuo@gmail.com>
2019-03-19 23:11:12 +0800

    bitly demo

--- com/coderli/py/analysis/bitly_ana.py
+++ com/coderli/py/analysis/bitly_ana.py
@@ -8,8 +8,8 @@ time_zones = [rec['tz'] for rec in records if 'tz' in rec]
 # print(time_zones[:10])
 
 from pandas import DataFrame, Series
+import numpy as np
 # import pandas as pd
-# import numpy as np
 data_frame = DataFrame(records)
 # 计数
 # print(data_frame["tz"].value_counts()[:10])
@@ -18,7 +18,19 @@ clean_tz = data_frame["tz"].fillna("Missing")
 print(clean_tz[:14])
 clean_tz[clean_tz == ""] = "Unknown"
 print(clean_tz[:14])
-import matplotlib.pyplot as plt
-clean_tz.value_counts()[:10].plot(kind="barh",rot=0)
+# 浏览器、设备、应用程序信息
+print(data_frame["a"][50])
 
-print(123)
+_in_frame = [x.split()[0] for x in data_frame.a.dropna()]
+results = Series(_in_frame)
+print(results[:5])
+print(results.value_counts()[:8])
+
+c_frame = data_frame[data_frame.a.notnull()]
+print(c_frame["a"][:5])
+o_sys = np.where(c_frame["a"].str.contains("Windows"),"Windows","Not Windows")
+print(o_sys[:10])
+
+# import matplotlib.pyplot as plt
+# clean_tz.value_counts()[:10].plot(kind="barh",rot=0)
+# plt.show()
